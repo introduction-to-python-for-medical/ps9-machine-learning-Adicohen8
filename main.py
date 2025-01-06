7b20560b2a902af0d35d97f09191d5a278facc50
Adicohen8 <mmade072@gmail.com>
2025-01-06 19:10:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import pandas as pd
+df = pd.read_csv('parkinsons.csv')
+df.head()
 
+features = ['PPE', 'DFA']
+target = 'status'
+x = df[features]
+y = df[target]
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+x = scaler.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+
+from sklearn.svm import SVC
+model = SVC()
+model.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score
+y_pred = model.predict(x_val)
+accuracy = accuracy_score(y_val, y_pred)
+print(f'Accuracy: {accuracy}')
